cbe5d7f6a74249c5a2b107906ad827bc5df8913a
Juliiaks <juliakharetonchuk@gmail.com>
2024-08-16 22:50:09 +0300

    create hw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,18 +2,21 @@ import { Router } from "express";
 // import { getAllContacts, getContactById } from "../services/contacts";
 import { getContactsController, getContactByIdController, createContactsController, deleteContactController, patchContactController } from "../controllers/contacts.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
+import { validateBody } from "../middlewares/validateBody.js";
+import { createContactSchema, updatContactSchema } from "../validation/contacts.js";
+import { isValidId } from "../middlewares/isValidId.js";
 
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/contacts/:contactId',isValidId, ctrlWrapper(getContactByIdController));
   
-router.post('/contacts', ctrlWrapper(createContactsController));
+router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactsController));
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch('/contacts/:contactId',isValidId, validateBody(updatContactSchema), ctrlWrapper(patchContactController));
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,18 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    phoneNumber:Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid("work", "home", "personal").required(),
+});
+
+export const updatContactSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    phoneNumber:Joi.string().min(3).max(20),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid("work", "home", "personal"),
+});
+
