389dd9b03b2533ccdc4e51133a836a2c55abe587
Juliiaks <juliakharetonchuk@gmail.com>
2024-09-05 18:55:25 +0300

    start hw6

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,6 +2,8 @@ import { getAllContacts, getContactById, createContact, deleteContact, updateCon
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from "../utils/parsePaginationParams.js";
 import { parseSortParams } from "../utils/parseSortParams.js";
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getContactsController = async (req, res) => {
     const { page, perPage } = parsePaginationParams(req.query);
@@ -59,7 +61,23 @@ export const deleteContactController = async (req, res, next) => {
 
 export const patchContactController = async (req, res, next) => {
     const { contactId } = req.params;
-    const result = await updateContact(contactId, req.body);
+    const photo = req.file;
+
+  let photoUrl;
+
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+
+    const result = await updateContact(contactId,{
+    ...req.body,
+    photo: photoUrl,
+  });
      if (!result) {
     next(createHttpError(404, 'Contact not found'));
     return;
