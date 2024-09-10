73e64f6502ee9b99c51ef1eb6c2bbb751d7fe50f
Juliiaks <juliakharetonchuk@gmail.com>
2024-09-10 21:15:33 +0300

    update

--- src/server.js
+++ src/server.js
@@ -43,13 +43,15 @@ export const setupServer = () => {
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
+  
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+  
 };
 
 // const app = express();
